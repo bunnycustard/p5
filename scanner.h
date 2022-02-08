d7265abe3841a4dae9423b4d896dfe6099eb0de9
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-02-08 15:17:04 -0700

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -3,33 +3,29 @@
 #include <fstream>
 #include <queue>
 
-enum state {init, sChar, sSpecial, sFinal, sEOF, sID, sComment, sString, sWhiteSpace};
+enum state{init, WhiteSpaceState, CharState, SpecialState, FinalState, IDState, CommentState, StringState, EOFState};
 
-class Scanner {
-    public:
-        // Functions
-        Scanner(string nameOfFile);
-        ~Scanner();
-        void tokenize();
-        queue <Token> GetList();
-
-        //variables
-        queue <Token> tokenList;
+class Scanner{
     protected:
-        //functions
         Token StateMachine();
         int LineGenerator();
         string WordExtractor();
         bool IsWhiteSpace();
 
-        //variables
         ifstream file;
         string fileName;
-        char currentChar;
-        char nextChar;
+        char char1;
+        char char2;
         int currentLine;
         state currentstate;
         string facts;
-        bool nextCharVal;
-        //int initial;
+        bool char2val;
+
+    public:
+        Scanner(string nameOfFile);
+        ~Scanner();
+        void tokenize();
+        queue <Token> GetList();
+
+        queue <Token> tokenList;
 };
