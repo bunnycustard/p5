b6cb9f92d1523e8054e254d00c455b52eb66df8d
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-02-08 18:23:58 -0700

    Update rules.cpp

--- rules.cpp
+++ rules.cpp
@@ -1,13 +1,28 @@
-#pragma once
+#include "rules.h"
 
-#include "predicate.h"
 
-class Rule{
-    private:
-        Predicate Head;
-        vector<Predicate> RuleList;
-    public:
-        void HeadPredicateSetter(Predicate head);
-        void RuleSetter(vector<Predicate> T);
-        string ToString();
-};
+void Rule::HeadPredicateSetter(Predicate head) {
+    Head = head;
+}
+
+void Rule::RuleSetter(vector<Predicate> Toke) {
+    for (unsigned int i = 0; i < Toke.size(); i++) {
+        RuleList.push_back(Toke.at(i));
+    }
+}
+
+string Rule::ToString() {
+    string theRule;
+    theRule = Head.ToString() + " :- ";
+    for (unsigned int i = 0; i < RuleList.size(); i++) {
+        if ( i == 0) {
+            theRule = theRule + RuleList.at(i).ToString();
+        }
+        else {
+            theRule = theRule + "," + RuleList.at(i).ToString();
+        }
+
+    } 
+    theRule += ".";
+    return theRule;
+}
