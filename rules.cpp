6755ddde5f31781f6c0495e6b7dbd199a15b3f8e
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-02-08 18:34:25 -0700

    Update rules.cpp

--- rules.cpp
+++ rules.cpp
@@ -1,28 +1,17 @@
 #include "rules.h"
 
-
-void Rule::SetHeadPredicate(Predicate head) {
-    Head = head;
-}
-
-void Rule::SetRule(vector<Predicate> Toke) {
-    for (unsigned int i = 0; i < Toke.size(); i++) {
-        RuleList.push_back(Toke.at(i));
-    }
+void Rule::HPSetter(Predicate head) {Head = head;}
+void Rule::RuleSetter(vector<Predicate> Toke) {
+    for (unsigned int i = 0; i < Toke.size(); i++) {RuleList.push_back(Toke.at(i));}
 }
 
 string Rule::ToString() {
-    string theRule;
-    theRule = Head.ToString() + " :- ";
+    string Rule1;
+    Rule1 = Head.ToString() + " :- ";
     for (unsigned int i = 0; i < RuleList.size(); i++) {
-        if ( i == 0) {
-            theRule = theRule + RuleList.at(i).ToString();
-        }
-        else {
-            theRule = theRule + "," + RuleList.at(i).ToString();
-        }
-
+        if ( i == 0) {Rule1 = Rule1 + RuleList.at(i).ToString();}
+        else{Rule1 = Rule1 + "," + RuleList.at(i).ToString();}
     } 
-    theRule += ".";
-    return theRule;
+    Rule1 += ".";
+    return Rule1;
 }
