4b78f8e5d550e5479f5b54a64d3f12d21b2f7e63
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-07 18:15:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "scanner.h"
 #include "parser.h"
+#include "interpreter.h"
 
 int main(int argc, char* argv[]){
     if(!argv[1]){
@@ -11,8 +12,9 @@ int main(int argc, char* argv[]){
         Scanner Scanner1(filename);  
         Scanner1.tokenize();
         Parser Parser1;
+				Interpreter Interpreter1;
         bool valid = Parser1.Logger(Scanner1.GetList());
-        if(valid == false){Parser1.ToString();}
+        if(valid == false){Interpreter1.dlSet(Parser1.GetDatalog);}
     }
     return 0;
 }
