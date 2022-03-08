ea2dc64beba618193b6015b3e186e242d103f0f9
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-07 18:16:23 -0700

    Update parameter.cpp

--- parameter.cpp
+++ parameter.cpp
@@ -1,4 +1,7 @@
 #include "parameter.h"
 
-void Parameter::ParamSetter(string Toke){P1 = Toke;}
+void Parameter::ParamSetter(string Toke){
+	P1 = Toke;
+	return;
+}
 string Parameter::ToString(){return P1;}
