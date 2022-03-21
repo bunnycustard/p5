6a843ba459a69f825fd7164f43d7484157bca89a
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-21 17:22:56 -0600

    Update rules.h

--- rules.h
+++ rules.h
@@ -1,23 +1,12 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include <vector>
+#include "predicate.h"
 
-using namespace std;
-
-class Scheme{
-	public:
-	vector <string> values;
-	unsigned int Size(){return values.size();}
-	void sGet(vector<string> placeholder){values = placeholder;}
-	string At(int i){return values.at(i);}
-	void pBack(string appender){
-		values.push_back(appender);
-		return;
-	}
-	string Fake(){
-		string opstr1;
-		for(unsigned int i = 0; i < values.size(); i ++){opstr1 += values.at(i);}
-		return opstr1;
-	}
+class Rule{
+    public:
+        void HPSetter(Predicate head);
+        void RuleSetter(vector<Predicate> Toke);
+        Predicate Head;
+        vector<Predicate> RuleList;
+				
+        string ToString();
 };
