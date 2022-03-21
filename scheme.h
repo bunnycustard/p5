54373eda78f4c48bf303f139c9536acbb0ba0cdd
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-21 17:23:19 -0600

    Update scheme.h

--- scheme.h
+++ scheme.h
@@ -6,13 +6,18 @@
 using namespace std;
 
 class Scheme{
-	vector <string> values;
 	public:
+	vector <string> values;
 	unsigned int Size(){return values.size();}
-	void sGet(vector<string> placeholder){values = 		 placeholder;}
+	void sGet(vector<string> placeholder){values = placeholder;}
 	string At(int i){return values.at(i);}
 	void pBack(string appender){
 		values.push_back(appender);
 		return;
+	}
+	string Fake(){
+		string opstr1;
+		for(unsigned int i = 0; i < values.size(); i ++){opstr1 += values.at(i);}
+		return opstr1;
 	}
 };
